4fbed8a96477c97a9378bfac9faad381ec82667c
ovezthaking <oliwerx12@gmail.com>
2025-06-07 18:52:18 +0200

    ignore

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://klxvvtjqeeebjcskvtvz.supabase.co'
-const supabaseKey = 'YourKeyHere' // Replace with your actual Supabase key
+const supabaseKey = 'YOUR_SUPABASE_SERVICE_ROLE_KEY' // Replace with your actual service role key
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
