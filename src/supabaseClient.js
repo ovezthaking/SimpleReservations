91efee57f4081ff92cecaf8f9a7aced125523524
ovezthaking <oliwerx12@gmail.com>
2025-06-07 19:11:33 +0200

    unhide

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://klxvvtjqeeebjcskvtvz.supabase.co'
-const supabaseKey = 'YOUR_SUPABASE_SERVICE_ROLE_KEY' // Replace with your actual service role key
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtseHZ2dGpxZWVlYmpjc2t2dHZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwMzYyNjQsImV4cCI6MjA2NDYxMjI2NH0.ITSDOxSTlV9qGj3lu0JIDhQiPlMR9kQapZqIQQD4loY'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
